8383cd24f1abed49575981fa779d98cc51c38c3a
Vuzeta95 <machnio95@o2.pl>
2018-02-19 20:00:48 +0100

    invite
    .

--- app.js
+++ app.js
@@ -27,49 +27,48 @@ function createLI(text) {
 
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const text = input.value;
-  input.value = '';
-  const li = createLI(text);
-  ul.appendChild(li);
-  
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const li = createLI(text);
+    ul.appendChild(li);
+
 });
 
 ul.addEventListener('change', (e) => {
-  const checkbox = event.target;
-  const checked = checkbox.checked;
-  const listItem = checkbox.parentNode.parentNode;
-  
-if(checked) {
-    listItem.className = "responded";
-  
-}else {
-    listItem.className = '';
-}  
-});
+    const checkbox = event.target;
+    const checked = checkbox.checked;
+    const listItem = checkbox.parentNode.parentNode;
+
+    if(checked) {
+        listItem.className = "responded";
+    }else {
+        listItem.className = '';
+    }  
+    });
 
 ul.addEventListener('click', (e) =>{
- if (e.target.tagName === 'BUTTON') {
-    const button = e.target; 
-    const li = e.target.parentNode;
-    const ul = li.parentNode;
- if(button.textContent === 'Remove') {  
-    ul.removeChild(li); 
-} else if(button.textContent === 'Edit') {
-    const span = li.firstElementChild;
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.value = span.textContent;
-    li.insertBefore(input, span);
-    li.removeChild(span);
-    button.textContent = 'Save';   
- } else if(button.textContent === 'Save') {
-    const input = li.firstElementChild;
-    const span = document.createElement('span');
-    span.textContent = input.value;
-    li.insertBefore(span, input);
-    li.removeChild(input);
-    button.textContent = 'Edit';   
- }
- }
+    if (e.target.tagName === 'BUTTON') {
+        const button = e.target; 
+        const li = e.target.parentNode;
+        const ul = li.parentNode;
+        if(button.textContent === 'Remove') {  
+            ul.removeChild(li); 
+        } else if(button.textContent === 'Edit') {
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = 'Save';   
+        } else if(button.textContent === 'Save') {
+            const input = li.firstElementChild;
+            const span = document.createElement('span');
+            span.textContent = input.value;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'Edit';   
+        }
+    }
 });
